def25fe1f3aaaa7f6efa8cb0d05b468a0a4233bf
Kiran  Shatiya <kiranshatiya@Kirans-MacBook-Pro.local>
2022-11-23 17:37:25 -0500

    Assignment 9 changes

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ import UserController
   from "./controllers/users/users-controller.js"
 import TuitsController
   from "./controllers/tuits/tuits-controller.js";
+import mongoose from "mongoose";
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://localhost:27017/tuiter'
+mongoose.connect(CONNECTION_STRING);
 const app = express()
 app.use(cors())
 app.use(express.json());
