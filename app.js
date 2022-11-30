24ac6fc44da041c0a0e51c2a9860d23c63bc4978
Kiran  Shatiya <kiranshatiya@Kirans-MacBook-Pro.local>
2022-11-30 18:41:58 -0500

    Adding minor punctuation

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
     || 'mongodb://localhost:27017/tuiter'
 mongoose.connect(CONNECTION_STRING);
 const app = express()
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 TuitsController(app);
 UserController(app)
