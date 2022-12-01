3a13a405461998c9c1cfd2d6a46f9d50bdca3997
Kiran  Shatiya <kiranshatiya@Kirans-MacBook-Pro.local>
2022-11-30 20:50:02 -0500

    Modifying tuits schema

--- controllers/tuits/tuits-schema.js
+++ controllers/tuits/tuits-schema.js
@@ -1,7 +1,15 @@
 import mongoose from 'mongoose';
 const schema = mongoose.Schema({
+  topic: String,
+  username: String,
+  time: String,
+  image: String,
+  title: String,
   tuit: String,
   likes: Number,
   liked: Boolean,
+  dislikes: Number,
+  replies: Number,
+  retuits: Number,
 }, {collection: 'tuits'});
 export default schema;
